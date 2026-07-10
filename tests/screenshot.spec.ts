cbb0035bf7f472b130835026abcbb17c6d90c855
Prasad Patil <prasad@incubyte.co>
2026-07-10 16:37:50 +0530

    Add screenshot test

---
+++ tests/screenshot.spec.ts
@@ -0,0 +1,29 @@
+import { test } from "@playwright/test";
+
+test("Screenshot", async ({ page }) => {
+  await page.goto("https://demowebshop.tricentis.com/");
+  // Date.now() returns the number of milliseconds elapsed since January 1, 1970, 00:00:00 UTC.
+  // This is commonly used to generate unique timestamps for filenames or other purposes where a unique identifier is needed based on the current time.
+  const timestamp = Date.now();
+  // Take a screenshot of the entire page
+  await page.screenshot({
+    path: `screenshots/full-page-${timestamp}.png`,
+    // ${timestamp} ensures unique filenames, here $ is used to insert the timestamp variable into the filename and {} is used to denote that it's a variable.
+    // This way, each screenshot will have a unique name based on the time it was taken.
+    fullPage: true,
+  });
+  // Take a screenshot of a specific element
+  const element = await page.$(".header-menu");
+  if (element) {
+    await element.screenshot({
+      path: `screenshots/header-menu-${timestamp}.png`,
+    });
+  }
+  // Take a screenshot of the viewport
+  await page.screenshot({ path: `screenshots/viewport-${timestamp}.png` });
+  // Take a screenshot of a specific area
+  await page.screenshot({
+    path: `screenshots/specific-area-${timestamp}.png`,
+    clip: { x: 0, y: 0, width: 800, height: 600 },
+  });
+});
